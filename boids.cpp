fc3b1228a1b84014e2ae2e837b12dbd6a2ea5fc4
x4A81 <awesometigerpenguin@gmail.com>
2025-03-02 15:14:38 +0800

    Added separation and alignment rule

--- boids.cpp
+++ boids.cpp
@@ -1,10 +1,14 @@
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <cmath>
 
 #define WIDTH 800
 #define HEIGHT 800
 #define NUM_BOIDS 50
 #define MOVE_SPEED 3
+#define NEIGHBOURS_DIST 50
+#define SEPARATION_DIST 15
+#define SEPARATION_FORCE 5
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -24,46 +28,120 @@ void init_boids() {
     }
 }
 
+float normalize_angle(float angle) {
+    return angle - 2 * M_PI * floor((angle + M_PI) / (2 * M_PI));
+}
+
+float calc_distance(int this_boid, int other_boid) {
+    float dist_x = boids[other_boid].x - boids[this_boid].x;
+    float dist_y = boids[other_boid].y - boids[this_boid].y;
+    return sqrt(dist_x * dist_x + dist_y * dist_y);
+}
+
+void separation(int this_boid, float* dx, float* dy) {
+    *dx = 0, *dy = 0;
+    for (int boid = 0; boid < NUM_BOIDS; boid++) {
+        float dist_x = boids[boid].x - boids[this_boid].x;
+        float dist_y = boids[boid].y - boids[this_boid].y;
+        if (boid == this_boid) continue;
+        if (calc_distance(this_boid, boid) < SEPARATION_DIST) {
+            *dx += (dist_x / calc_distance(this_boid, boid)) * SEPARATION_FORCE;
+            *dy += (dist_y / calc_distance(this_boid, boid)) * SEPARATION_FORCE;
+        }
+    }
+}
+
+void alignment(int this_boid, float* heading) {
+    float avg_heading = 0, count = 1;
+    for (int boid = 0; boid < NUM_BOIDS; boid++) {
+        if (boid == this_boid) continue;
+        if (calc_distance(this_boid, boid) < NEIGHBOURS_DIST) {
+            avg_heading += boids[boid].angle;
+            count++;
+        }
+    }
+
+    *heading = avg_heading / count;
+    
+}
+
 void update_boids() {
     for (int boid = 0; boid < NUM_BOIDS; boid++) {
+        float dx = 0, dy = 0, heading = 0;
+        separation(boid, &dx, &dy);
+
+        if (dx != 0 || dy != 0) {
+            float heading = atan2(dy, dx);
+            float diff = heading - boids[boid].angle;
+            boids[boid].angle += normalize_angle(diff) * 0.1f;
+        }
+
+        alignment(boid, &heading);
+        if (heading != 0) {
+            float diff = heading - boids[boid].angle;
+            boids[boid].angle += normalize_angle(diff) * 0.1f;
+        }
+
         float theta = boids[boid].angle;
         boids[boid].x += MOVE_SPEED * cos(theta);
         boids[boid].y += MOVE_SPEED * sin(theta);
 
         // Boundary conditions
-        int turn_around = 0;
         if (boids[boid].x >= WIDTH) {
-            boids[boid].x = WIDTH;
-            turn_around = 1;
-        }
-        
-        if (boids[boid].x <= 0) {
-            boids[boid].x = 0;
-            turn_around = 1;
+            boids[boid].x = 5;
+        } else if (boids[boid].x <= 0) {
+            boids[boid].x = WIDTH-5;
         }
 
         if (boids[boid].y >= HEIGHT) {
-            boids[boid].y = HEIGHT;
-            turn_around = 1;
+            boids[boid].y = 5;
+        } else if (boids[boid].y <= 0) {
+            boids[boid].y = HEIGHT-5;
         }
 
-        if (boids[boid].y <= 0) {
-            boids[boid].y = 0;
-            turn_around = 1;
-        }
-
-        if (turn_around) {
-            boids[boid].angle += M_PI;
-        }
-        
         boids[boid].angle += ((rand() % 3 - 1) * 0.1f);
+        if (boid == 10) {
+            SDL_Log("X: %f, Y: %f, Angle: %f", boids[boid].x, boids[boid].y, boids[boid].angle);
+        }
     }
 }
 
 void draw_boids() {
+    const int BOID_SIZE = 10;  // Adjust this value to change boid size
+    SDL_Vertex vertices[3];  // Three vertices for each triangle
+    
+    // Set common vertex properties
     for (int boid = 0; boid < NUM_BOIDS; boid++) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderPoint(renderer, boids[boid].x, boids[boid].y);
+        for (int i = 0; i < 3; i++) {
+            if (boid == 10) {
+                vertices[i].color.r = 1.0f;  // Red
+                vertices[i].color.g = 0.0f;
+                vertices[i].color.b = 0.0f;
+                vertices[i].color.a = 1.0f;
+            } else {
+                vertices[i].color.r = 1.0f;  // White
+                vertices[i].color.g = 1.0f;
+                vertices[i].color.b = 1.0f;
+                vertices[i].color.a = 1.0f;
+            }
+            vertices[i].tex_coord.x = 0;
+            vertices[i].tex_coord.y = 0;
+        }
+
+        // Front point (nose of the triangle)
+        vertices[0].position.x = boids[boid].x + BOID_SIZE * cos(boids[boid].angle);
+        vertices[0].position.y = boids[boid].y + BOID_SIZE * sin(boids[boid].angle);
+        
+        // Back points with wider spread
+        const float backAngle = 2.5f;  // Controls triangle width
+        vertices[1].position.x = boids[boid].x + (BOID_SIZE * 0.5f) * cos(boids[boid].angle + backAngle);
+        vertices[1].position.y = boids[boid].y + (BOID_SIZE * 0.5f) * sin(boids[boid].angle + backAngle);
+        
+        vertices[2].position.x = boids[boid].x + (BOID_SIZE * 0.5f) * cos(boids[boid].angle - backAngle);
+        vertices[2].position.y = boids[boid].y + (BOID_SIZE * 0.5f) * sin(boids[boid].angle - backAngle);
+        
+        // Draw the filled triangle
+        SDL_RenderGeometry(renderer, NULL, vertices, 3, NULL, 0);
     }
 }
 
